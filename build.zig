5d26a9e85808cae248ffd85059b74c32a6f3f50e
GreenTea2626 <frogperson@web.de>
2026-01-28 23:58:26 +0100

    fix: move source build

--- build.zig
+++ build.zig
@@ -5,9 +5,70 @@ pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zignav", .{
+    // Create the Zig module
+    const zignav = b.addModule("root", .{
         .root_source_file = b.path("main.zig"),
-        .target = target,
-        .optimize = optimize,
+    });
+    zignav.addIncludePath(b.path("Recast/Include"));
+    zignav.addIncludePath(b.path("Detour/Include"));
+    zignav.addIncludePath(b.path("DetourTileCache/Include"));
+    zignav.addIncludePath(b.path("DetourCrowd/Include"));
+
+    // Create a static library for the C++ code
+    const zignav_c_cpp = b.addLibrary(.{
+        .name = "zignav_c_cpp",
+        .linkage = .static,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    b.installArtifact(zignav_c_cpp);
+
+    zignav_c_cpp.root_module.addIncludePath(b.path("Recast/Include"));
+    zignav_c_cpp.root_module.addIncludePath(b.path("Detour/Include"));
+    zignav_c_cpp.root_module.addIncludePath(b.path("DetourTileCache/Include"));
+    zignav_c_cpp.root_module.addIncludePath(b.path("DetourCrowd/Include"));
+    zignav_c_cpp.root_module.link_libc = true;
+    zignav_c_cpp.root_module.link_libcpp = true;
+
+    zignav_c_cpp.root_module.addCSourceFiles(.{
+        .files = &.{
+            "Recast/Include/Recast_glue.cpp",
+            "Recast/Source/Recast.cpp",
+            "Recast/Source/RecastAlloc.cpp",
+            "Recast/Source/RecastArea.cpp",
+            "Recast/Source/RecastAssert.cpp",
+            "Recast/Source/RecastContour.cpp",
+            "Recast/Source/RecastFilter.cpp",
+            "Recast/Source/RecastLayers.cpp",
+            "Recast/Source/RecastMesh.cpp",
+            "Recast/Source/RecastMeshDetail.cpp",
+            "Recast/Source/RecastRasterization.cpp",
+            "Recast/Source/RecastRegion.cpp",
+            "Detour/Include/DetourAlloc_glue.cpp",
+            "Detour/Include/DetourAssert_glue.cpp",
+            "Detour/Include/DetourCommon_glue.cpp",
+            "Detour/Include/DetourNavMesh_glue.cpp",
+            "Detour/Include/DetourNavMeshBuilder_glue.cpp",
+            "Detour/Include/DetourNavMeshQuery_glue.cpp",
+            "Detour/Include/DetourNode_glue.cpp",
+            "Detour/Include/DetourStatus_glue.cpp",
+            "Detour/Source/DetourAlloc.cpp",
+            "Detour/Source/DetourAssert.cpp",
+            "Detour/Source/DetourCommon.cpp",
+            "Detour/Source/DetourNavMesh.cpp",
+            "Detour/Source/DetourNavMeshBuilder.cpp",
+            "Detour/Source/DetourNavMeshQuery.cpp",
+            "Detour/Source/DetourNode.cpp",
+            "DetourTileCache/Include/DetourTileCache_glue.cpp",
+            "DetourTileCache/Include/DetourTileCacheBuilder_glue.cpp",
+            "DetourTileCache/Source/DetourTileCache.cpp",
+            "DetourTileCache/Source/DetourTileCacheBuilder.cpp",
+            "DetourCrowd/Include/DetourPathCorridor_glue.cpp",
+            "DetourCrowd/Source/DetourPathCorridor.cpp",
+        },
+        .flags = &.{},
     });
 }
--- zigsrc/build.zig
+++ zigsrc/build.zig
@@ -20,53 +20,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe.root_module.addImport("zignav", zignav_dep.module("zignav"));
-
-    exe.linkLibCpp();
-    exe.addIncludePath(zignav_dep.path("Recast/Include"));
-    exe.addIncludePath(zignav_dep.path("Detour/Include"));
-    exe.addIncludePath(zignav_dep.path("DetourTileCache/Include"));
-    exe.addIncludePath(zignav_dep.path("DetourCrowd/Include"));
-
-    exe.addCSourceFiles(.{
-        .root = zignav_dep.path(""),
-        .files = &.{
-            "Recast/Include/Recast_glue.cpp",
-            "Recast/Source/Recast.cpp",
-            "Recast/Source/RecastAlloc.cpp",
-            "Recast/Source/RecastArea.cpp",
-            "Recast/Source/RecastAssert.cpp",
-            "Recast/Source/RecastContour.cpp",
-            "Recast/Source/RecastFilter.cpp",
-            "Recast/Source/RecastLayers.cpp",
-            "Recast/Source/RecastMesh.cpp",
-            "Recast/Source/RecastMeshDetail.cpp",
-            "Recast/Source/RecastRasterization.cpp",
-            "Recast/Source/RecastRegion.cpp",
-            "Detour/Include/DetourAlloc_glue.cpp",
-            "Detour/Include/DetourAssert_glue.cpp",
-            "Detour/Include/DetourCommon_glue.cpp",
-            "Detour/Include/DetourNavMesh_glue.cpp",
-            "Detour/Include/DetourNavMeshBuilder_glue.cpp",
-            "Detour/Include/DetourNavMeshQuery_glue.cpp",
-            "Detour/Include/DetourNode_glue.cpp",
-            "Detour/Include/DetourStatus_glue.cpp",
-            "Detour/Source/DetourAlloc.cpp",
-            "Detour/Source/DetourAssert.cpp",
-            "Detour/Source/DetourCommon.cpp",
-            "Detour/Source/DetourNavMesh.cpp",
-            "Detour/Source/DetourNavMeshBuilder.cpp",
-            "Detour/Source/DetourNavMeshQuery.cpp",
-            "Detour/Source/DetourNode.cpp",
-            "DetourTileCache/Include/DetourTileCache_glue.cpp",
-            "DetourTileCache/Include/DetourTileCacheBuilder_glue.cpp",
-            "DetourTileCache/Source/DetourTileCache.cpp",
-            "DetourTileCache/Source/DetourTileCacheBuilder.cpp",
-            "DetourCrowd/Include/DetourPathCorridor_glue.cpp",
-            "DetourCrowd/Source/DetourPathCorridor.cpp",
-        },
-        .flags = &.{},
-    });
+    exe.root_module.addImport("zignav", zignav_dep.module("root"));
+    exe.root_module.linkLibrary(zignav_dep.artifact("zignav_c_cpp"));
 
     b.installArtifact(exe);
 
